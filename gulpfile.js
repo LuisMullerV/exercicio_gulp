18e09e188652c60a60cc6322adbd5b4ac29ead0b
Muller <mullerventurin@gmail.com>
2025-08-12 18:58:12 -0300

    Adiciona gulpfile, package.json e estrutura do projeto

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
+const plumber = require('gulp-plumber');
+const del = require('del');
+
+const paths = {
+  styles: { src: 'src/scss/**/*.scss', dest: 'dist/css' },
+  scripts: { src: 'src/js/**/*.js', dest: 'dist/js' },
+  images: { src: 'src/images/**/*.{jpg,jpeg,png,gif,svg,webp}', dest: 'dist/images' }
+};
+
+function clean() {
+  return del(['dist/**', '!dist']);
+}
+
+function styles() {
+  return src(paths.styles.src)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(paths.styles.dest));
+}
+
+function scripts() {
+  return src(paths.scripts.src, { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest(paths.scripts.dest, { sourcemaps: '.' }));
+}
+
+function images() {
+  return src(paths.images.src)
+    .pipe(imagemin([
+      imagemin.mozjpeg({quality: 80, progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(dest(paths.images.dest));
+}
+
+function watcher() {
+  watch(paths.styles.src, styles);
+  watch(paths.scripts.src, scripts);
+  watch(paths.images.src, images);
+}
+
+const build = series(clean, parallel(styles, scripts, images));
+
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.watch = watcher;
+exports.build = build;
+exports.default = series(build, watcher);
---
+++ src/js/app.js
@@ -0,0 +1,4 @@
+(function () {
+  function add(a, b) { return a + b; }
+  console.log('App pronto! Soma de 2+3 =', add(2, 3));
+})();
